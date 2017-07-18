93b0aaa4b8050e78bc2efa0adfe1a8eed34c20e1
Haowei-Guo <haowei.guo@groupbyinc.com>
2017-07-18 15:37:20 -0400

    Add test

--- test/unit/range-input.ts
+++ test/unit/range-input.ts
@@ -1,8 +1,9 @@
+import { Selectors } from '@storefront/core';
 import RangeInput from '../../src/range-input';
 import suite from './_suite';
 
-suite('RangeInput', ({ expect, spy }) => {
-  let rangeInput: RangeInput;
+suite('RangeInput', ({ expect, spy, stub }) => {
+  let rangeInput;
 
   beforeEach(() => rangeInput = new RangeInput());
 
@@ -18,12 +19,79 @@ suite('RangeInput', ({ expect, spy }) => {
     });
 
     describe('init()', () => {
+      beforeEach(() => {
+        const state = {
+          data: {
+            navigations: {
+              byId: {
+                age_range: {
+                  label: 'Age Range'
+                }
+              }
+            }
+          }
+        };
+        rangeInput['flux'] = <any>{
+          store: {
+            getState: () => null
+          }
+        };
+        rangeInput.props = {
+          field: 'age_range'
+        };
+        rangeInput['label'] = '';
+        stub(rangeInput.flux.store, 'getState').returns(state);
+      });
+
       it('should expose rangeInput', () => {
         const expose = rangeInput.expose = spy();
 
         rangeInput.init();
 
         expect(expose).to.be.calledWith('rangeInput', rangeInput);
+      });
+
+      it('should set label', () => {
+        rangeInput.expose = () => null;
+
+
+        rangeInput.init();
+
+        expect(rangeInput.label).to.eq('Age Range');
+      });
+    });
+  });
+
+  describe('search()', () => {
+    it('should update search', () => {
+      rangeInput['actions'] = {
+        updateSearch: null
+      };
+      rangeInput.refs = {
+        low: {
+          value: '10'
+        },
+        high: {
+          value: '20'
+        }
+      };
+      const updateSearch = rangeInput.actions.updateSearch = spy();
+      const query = { a: 'b' };
+      rangeInput.props.field = 'Age Range';
+      stub(Selectors, 'query').returns(query);
+      rangeInput['flux'] = {
+        store: {
+          getState: () => null
+        }
+      };
+
+      rangeInput.search();
+
+      expect(updateSearch).to.be.calledWith({
+        query,
+        navigationId: 'Age Range',
+        low: 10,
+        high: 20
       });
     });
   });
