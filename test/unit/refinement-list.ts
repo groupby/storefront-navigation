67db23cc4848e42d8a812c56470892b8e597ee64
Youdongchen(Declan) Zhao <zydc2010@gmail.com>
2017-08-01 10:26:19 -0400

    test(coverage): Add tests for refinement-list (#17)

--- test/unit/refinement-list.ts
+++ test/unit/refinement-list.ts
@@ -1,11 +1,36 @@
 import RefinementList from '../../src/refinement-list';
 import suite from './_suite';
 
-suite('RefinementList', ({ expect }) => {
+suite('RefinementList', ({ expect, spy }) => {
+  let refinementList: RefinementList;
+
+  beforeEach(() => refinementList = new RefinementList());
 
   describe('constructor()', () => {
-    it('should be ok', () => {
-      expect(() => new RefinementList()).to.not.throw();
+    describe('props', () => {
+      it('should set initial value', () => {
+        expect(refinementList.props).eql(<any>{ refinements: [] });
+      });
+    });
+  });
+
+  describe('init()', () => {
+    it('should expose refinements', () => {
+      const expose = refinementList.expose = spy();
+
+      refinementList.init();
+
+      expect(expose).to.be.calledWith('refinements', refinementList.props.refinements);
+    });
+  });
+
+  describe('onUpdate()', () => {
+    it('should call updateAlias()', () => {
+      const updateAlias = refinementList.updateAlias = spy();
+
+      refinementList.onUpdate();
+
+      expect(updateAlias).to.be.calledWith('refinements', refinementList.props.refinements);
     });
   });
 });
