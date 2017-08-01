10796b5606c6fc5ebf4d373d55c54d2add452afe
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-01 00:11:32 -0400

    test(package): switch to @storefront/testing

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,12 +1,10 @@
+import { bootstrap } from '@storefront/testing';
 import * as chai from 'chai';
-import * as mock from 'mock-require';
-import * as sinonChai from 'sinon-chai';
 
-chai.use(sinonChai);
-
-mock('../src/navigation/index.html', {});
-mock('../src/navigation-list/index.html', {});
-mock('../src/range-refinement-controls/index.html', {});
-mock('../src/refinement/index.html', {});
-mock('../src/refinement-list/index.html', {});
-mock('../src/value-refinement-controls/index.html', {});
+bootstrap(chai, __dirname, ['../src/navigation/index.html',
+  '../src/navigation-list/index.html',
+  '../src/range-refinement-controls/index.html',
+  '../src/refinement/index.html',
+  '../src/refinement-list/index.html',
+  '../src/value-refinement-controls/index.html'
+])
