c6f6cf55641f159e8ed3ea1dd5045fd3d71d2a24
Victoria Johns <victoria.johns@groupbyinc.com>
2017-08-14 14:17:49 -0400

    f

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -10,7 +10,7 @@ bootstrap(chai, __dirname, [
   '../src/range-refinement-controls/index.html',
   '../src/refinement/index.html',
   '../src/refinement-list/index.html',
-  '../src/filter-refinement-controls',
-  '../src/filtered-refinement-list',
+  '../src/filter-refinement-controls/index.html',
+  '../src/filtered-refinement-list/index.html',
   '../src/value-refinement-controls/index.html'
 ]);
