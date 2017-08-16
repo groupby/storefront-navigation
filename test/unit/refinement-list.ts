46f39ece9bb4d12568ed0386436f9846e1c871bc
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-16 18:41:34 -0400

    update tests

--- test/unit/refinement-list.ts
+++ test/unit/refinement-list.ts
@@ -1,7 +1,7 @@
 import RefinementList from '../../src/refinement-list';
 import suite from './_suite';
 
-suite('RefinementList', ({ expect, spy }) => {
+suite('RefinementList', ({ expect, spy, stub }) => {
   let refinementList: RefinementList;
 
   beforeEach(() => refinementList = new RefinementList());
@@ -14,15 +14,23 @@ suite('RefinementList', ({ expect, spy }) => {
     });
   });
 
+  describe('get alias()', () => {
+    it('should return alias name', () => {
+      expect(refinementList.alias).to.eq('refinements');
+    });
+  });
+
   describe('init()', () => {
     it('should expose refinements', () => {
       const expose = refinementList.expose = spy();
       const refinements: any = ['a', 'b'];
+      const alias = 'c';
+      stub(refinementList, 'alias').get(() => alias);
       refinementList.props = { refinements };
 
       refinementList.init();
 
-      expect(expose).to.be.calledWith('refinements', refinements);
+      expect(expose).to.be.calledWith(alias, refinements);
     });
   });
 
@@ -30,11 +38,13 @@ suite('RefinementList', ({ expect, spy }) => {
     it('should call updateAlias()', () => {
       const updateAlias = refinementList.updateAlias = spy();
       const refinements: any = ['a', 'b'];
+      const alias = 'c';
+      stub(refinementList, 'alias').get(() => alias);
       refinementList.props = { refinements };
 
       refinementList.onUpdate();
 
-      expect(updateAlias).to.be.calledWith('refinements', refinements);
+      expect(updateAlias).to.be.calledWith(alias, refinements);
     });
   });
 });
