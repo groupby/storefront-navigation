a9088dd62ccf970c2cb23f33d70919890a6fb05a
Filip Francetic <f.francet@hotmail.com>
2017-12-13 15:00:51 -0500

    make refinement pill a view

--- src/refinement-pill/index.ts
+++ src/refinement-pill/index.ts
@@ -1,7 +1,6 @@
-import { alias, tag, Events, ProductTransformer, Selectors, Store, Structure, Tag } from '@storefront/core';
+import { alias, view, Events, ProductTransformer, Selectors, Store, Structure, Tag } from '@storefront/core';
 
-@alias('refinementPill')
-@tag('gb-refinement-pill', require('./index.html'))
+view('gb-refinement-pill', require('./index.html'));
 class RefinementPill {
   props: RefinementPill.Props = {
     refinement: undefined,
@@ -11,16 +10,14 @@ class RefinementPill {
 interface RefinementPill extends Tag<RefinementPill.Props> { }
 namespace RefinementPill {
   export interface Props extends Tag.Props {
-    refinement: PillsRefinement;
+    refinement: Store.Refinement & {
+      onClick: Function,
+      onClose?: Function,
+      selected: boolean,
+      value: string,
+      display?: string,
+    };
   }
-
-  export type PillsRefinement = Store.Refinement & {
-    onClick: Function,
-    onClose?: Function,
-    selected: boolean,
-    value: string,
-    display?: string,
-  };
 }
 
 export default RefinementPill;
