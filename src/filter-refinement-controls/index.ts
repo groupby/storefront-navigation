3651b077d167fa8ca756efe1c8f7a3855037bde3
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-14 18:59:16 -0400

    improvements

--- src/filter-refinement-controls/index.ts
+++ src/filter-refinement-controls/index.ts
@@ -3,7 +3,6 @@ import ValueRefinementControls from '../value-refinement-controls';
 
 @tag('gb-filter-refinement-controls', require('./index.html'))
 class FilterRefinementControls extends ValueRefinementControls {
-
   get alias() {
     return 'filterControls';
   }
--- test/unit/refinement-pills-category.ts
+++ test/unit/refinement-pills-category.ts
@@ -3,13 +3,15 @@ import * as Sinon from 'sinon';
 import RefinementPillsCategory from '../../src/refinement-pills-category';
 import suite from './_suite';
 
-suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
+suite('RefinementPillsCategory', ({ expect, spy, stub, itShouldProvideAlias }) => {
   let refinementPillsCategory: RefinementPillsCategory;
 
   beforeEach(() => {
     refinementPillsCategory = new RefinementPillsCategory();
     refinementPillsCategory.props.storeSection = StoreSections.PAST_PURCHASES;
   });
+
+  itShouldProvideAlias(RefinementPillsCategory, 'refinementPillsCategory');
 
   describe('constructor()', () => {
     describe('props', () => {
@@ -33,7 +35,7 @@ suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
 
   describe('init()', () => {
     it('should call updateState()', () => {
-      const updateState = refinementPillsCategory.updateState = spy();
+      const updateState = (refinementPillsCategory.updateState = spy());
 
       refinementPillsCategory.init();
 
@@ -43,7 +45,7 @@ suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
 
   describe('onUpdate()', () => {
     it('should call updateState()', () => {
-      const updateState = refinementPillsCategory.updateState = spy();
+      const updateState = (refinementPillsCategory.updateState = spy());
       const state: any = { a: 1 };
       refinementPillsCategory.state = state;
 
@@ -60,12 +62,12 @@ suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
       const navigation: any = {
         field,
         selected: [1],
-        refinements: [{ a: 1, onClick }, { b: 2, }, { c: 3, }]
+        refinements: [{ a: 1, onClick }, { b: 2 }, { c: 3 }],
       };
       const newNavigation: any = {
         field,
         selected: [1],
-        refinements: [{ a: 1, selected: false, onClick }, { b: 2, selected: true }, { c: 3, selected: false }]
+        refinements: [{ a: 1, selected: false, onClick }, { b: 2, selected: true }, { c: 3, selected: false }],
       };
       const newRefinements: any = [
         {
@@ -82,17 +84,16 @@ suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
           c: 3,
           selected: false,
           onClick: () => this.actions['resetPastPurchaseQueryAndSelectRefinement'](field, 2),
-        }
+        },
       ];
       refinementPillsCategory.props.navigation = navigation;
       refinementPillsCategory.actions = <any>{};
-      const action = refinementPillsCategory.actions.resetPastPurchaseQueryAndSelectRefinement = spy();
+      const action = (refinementPillsCategory.actions.resetPastPurchaseQueryAndSelectRefinement = spy());
 
       refinementPillsCategory.updateState();
 
       expect(refinementPillsCategory.state.navigation).to.be.eql(newNavigation);
-      expect(JSON.stringify(refinementPillsCategory.state.refinements)).to.be
-        .eql(JSON.stringify(newRefinements));
+      expect(JSON.stringify(refinementPillsCategory.state.refinements)).to.be.eql(JSON.stringify(newRefinements));
       refinementPillsCategory.state.refinements[0]['onClick']();
       expect(onClick).to.be.calledOnce;
       refinementPillsCategory.state.refinements[1]['onClick']();
@@ -104,7 +105,7 @@ suite('RefinementPillsCategory', ({ expect, spy, stub }) => {
 
   it('should set state when navigation is undefined', () => {
     refinementPillsCategory.props.navigation = undefined;
-    const set = refinementPillsCategory.set = spy();
+    const set = (refinementPillsCategory.set = spy());
     refinementPillsCategory.actions = <any>{};
 
     refinementPillsCategory.updateState();
