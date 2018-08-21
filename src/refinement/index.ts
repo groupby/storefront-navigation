c78aabc96abbe029d4a495393a6dd50b11ecb023
Johann Tutor <johann.tutor@groupbyinc.com>
2018-08-21 17:07:50 -0400

    Allow enabling the count badge via a prop
    This prop is configurable so that it can be turned on for all Refinement
    components without making a custom component to override it.
    
    The feature is turned off by default to preserve the existing behavior.
    
    The component's state is aliased as `refinementDisplay` so that the
    total can be accessed by child components. The more intuitive
    `refinement` is already taken by the list item of `RefinementList`.

--- src/refinement/index.ts
+++ src/refinement/index.ts
@@ -1,11 +1,25 @@
-import { tag, Selectors, Tag } from '@storefront/core';
+import { configurable, provide, tag, Selectors, Tag } from '@storefront/core';
 import RefinementList from '../refinement-list';
 import ValueRefinementControls from '../value-refinement-controls';
 
+@configurable
+@provide('refinementDisplay')
 @tag('gb-refinement', require('./index.html'))
 class Refinement {
-  getTotal() {
-    return this.props.selected && !this.props.or ? this.select(Selectors.recordCount) : this.props.total;
+  props: Refinement.Props = <Refinement.Props>{
+    alwaysShowCount: false,
+  };
+
+  state: Refinement.State = {
+    total: 0,
+  };
+
+  init() {
+    this.updateTotal();
+  }
+
+  onUpdate() {
+    this.updateTotal();
   }
 
   onClick(event: Refinement.IndexedClickEvent) {
@@ -14,14 +28,26 @@ class Refinement {
       this.props.onClick();
     }
   }
+
+  updateTotal() {
+    this.state = {
+      ...this.state,
+      total: this.props.selected && !this.props.or ? this.select(Selectors.recordCount) : this.props.total,
+    }
+  }
 }
 
 interface Refinement extends Tag<Refinement.Props> {}
 namespace Refinement {
   export interface Props {
+    alwaysShowCount: boolean;
     onClick: () => void;
     or: boolean;
     selected: boolean;
+    total: number;
+  }
+
+  export interface State {
     total: number;
   }
 
--- test/unit/refinement.ts
+++ test/unit/refinement.ts
@@ -2,10 +2,49 @@ import { Selectors } from '@storefront/core';
 import Refinement from '../../src/refinement';
 import suite from './_suite';
 
-suite('Refinement', ({ expect, spy, stub }) => {
+suite('Refinement', ({ expect, spy, stub, itShouldProvideAlias }) => {
   let refinement: Refinement;
 
   beforeEach(() => (refinement = new Refinement()));
+
+  itShouldProvideAlias(Refinement, 'refinementDisplay');
+
+  describe('constructor()', () => {
+    describe('props', () => {
+      it('should set initial values', () => {
+        expect(refinement.props).to.eql({ alwaysShowCount: false });
+      });
+    });
+
+    describe('state', () => {
+      it('should set initial value', () => {
+        expect(refinement.state).to.eql({ total: 0 });
+      });
+    });
+  });
+
+  describe('init()', () => {
+    it('should set state', () => {
+      const total = 1024;
+      const updateTotal = refinement.updateTotal = spy();
+
+      refinement.init();
+
+      expect(updateTotal).to.be.called;
+    });
+  });
+
+  describe('onUpdate()', () => {
+    it('should update the total', () => {
+      const total = 1024;
+      const updateTotal = refinement.updateTotal = spy();
+      refinement.state = <any>{ total: 1 };
+
+      refinement.onUpdate();
+
+      expect(updateTotal).to.be.called;
+    });
+  });
 
   describe('onClick()', () => {
     it('should set preventUpdate', () => {
@@ -27,12 +66,14 @@ suite('Refinement', ({ expect, spy, stub }) => {
     });
   });
 
-  describe('getTotal()', () => {
+  describe('updateTotal()', () => {
     it('should return the total if it exists', () => {
       const total = 1024;
       refinement.props = <any>{ total };
 
-      expect(refinement.getTotal()).to.eq(total);
+      refinement.updateTotal();
+
+      expect(refinement.state).to.eql({ total });
     });
 
     it('should return the record count if the refinement is selected and not or-able', () => {
@@ -43,7 +84,9 @@ suite('Refinement', ({ expect, spy, stub }) => {
         selected: true,
       };
 
-      expect(refinement.getTotal()).to.eq(count);
+      refinement.updateTotal();
+
+      expect(refinement.state).to.eql({ total: count });
     });
   });
 });
