c322a8a00f523c5d3eff4cb54e3d03dbcd2a6b64
Dmytro <dmytro.kyiashko@gmail.com>
2018-09-24 15:18:26 +0300

    Add filter-refinement

--- src/refinement-list/index.ts
+++ src/refinement-list/index.ts
@@ -1,4 +1,5 @@
 import { tag, Store, Tag } from '@storefront/core';
+const RETURN_KEY_CODE = 13;
 
 @tag('gb-refinement-list', require('./index.html'))
 class RefinementList {
@@ -7,12 +8,40 @@ class RefinementList {
     itemProps: {},
   };
 
+  state={
+    filteredRefinements : []
+  }
+
+
+  handleChange(e){
+    const { target } = e;
+    this.set({
+      filteredRefinements : this.props.refinements.filter((refinement) => refinement.value.toLowerCase().includes(target.value.toLowerCase()))
+    })
+  }
+
+  handleSelect(e){
+    const { keyCode, target } = e;
+    if(keyCode === RETURN_KEY_CODE) {
+      const foundItem = this.state.filteredRefinements.find(
+        (filteredRefinement) => filteredRefinement.value.toLowerCase() === target.value.toLowerCase());
+
+      if(foundItem && foundItem.onClick) {
+        foundItem.onClick();
+      }
+    }
+  }
+
+
   get alias() {
     return 'refinements';
   }
 
   init() {
     this.provide(this.alias, ({ refinements }) => refinements);
+    this.state={
+      filteredRefinements : this.props.refinements
+    }
   }
 }
 
@@ -23,7 +52,7 @@ namespace RefinementList {
     itemProps?: object;
   }
 
-  export type Refinement = Store.Refinement & { selected: boolean };
+  export type Refinement = Store.Refinement & { selected: boolean, value: string };
 }
 
 export default RefinementList;
--- test/unit/refinement-list.ts
+++ test/unit/refinement-list.ts
@@ -24,6 +24,7 @@ suite('RefinementList', ({ expect, spy, stub }) => {
   describe('init()', () => {
     it('should provide refinements', () => {
       const provide = (refinementList.provide = spy());
+      refinementList.set = () => null;
       const refinements: any = ['a', 'b'];
       const alias = 'c';
       stub(refinementList, 'alias').get(() => alias);
@@ -31,7 +32,47 @@ suite('RefinementList', ({ expect, spy, stub }) => {
 
       refinementList.init();
 
+      const newState = {filteredRefinements : refinements};
+
       expect(provide).to.be.calledWithExactly(alias, sinon.match.func);
+      expect(refinementList.state).to.eql(newState);
+    });
+  });
+
+  describe('handleChange()', () => {
+    it('should filter refinements', () => {
+      const set = refinementList.set = spy();
+      const refinements: any = [{value : 'a'}, {value : 'b'}];
+      refinementList.props = { refinements };
+      const keyboardEvent = {target : {value : 'a'} }
+
+      refinementList.handleChange(keyboardEvent);
+
+      expect(set).to.be.calledWith({
+        filteredRefinements : [{value : 'a'}]
+      });
+    });
+  });
+
+  describe('handleSelect()', () => {
+    it('should select refinement', () => {
+      const refinements: any = [{value : 'a' , onClick : spy()}, {value : 'b', onClick : spy()}];
+      refinementList.state = { filteredRefinements : refinements };
+      const keyboardEvent = {target : {value : 'a'} , keyCode : 13 }
+      refinementList.handleSelect(keyboardEvent);
+
+      expect(refinements[0].onClick).to.be.called
+      expect(refinements[1].onClick).to.be.not.called
+    });
+
+    it('should not select refinement', () => {
+      const refinements: any = [{value : 'a' , onClick : spy()}, {value : 'b', onClick : spy()}];
+      refinementList.state = { filteredRefinements : refinements };
+      const keyboardEvent = {target : {value : 'a'} , keyCode : 15 }
+      refinementList.handleSelect(keyboardEvent);
+
+      expect(refinements[0].onClick).to.be.not.called
+      expect(refinements[1].onClick).to.be.not.called
     });
   });
 });
